6e106b90b458ca2f1c051d350398fcb760e47d0e
imshubham27 <shubhsharma.2708@gmail.com>
2020-04-09 23:01:33 +0530

    Updated

---
+++ Basics/myscript.js
@@ -0,0 +1,19 @@
+// x=0;
+// while(x<11){
+//     if(x%2===0){
+//         console.log(x);
+//     }
+//     x++;
+// }
+flag=0;
+var a,b,c,d,e;
+a=prompt("Enter your first name: ");
+b=prompt("Enter your last name: ")
+c=prompt("AGE");
+d=prompt("Height");
+e=prompt("Pet name: ");
+f=e.length;
+if(a[0]===b[0] && (c<30 && c>20) && d>=170 && e[f-1]==="y"){
+    console.log("You are a spy");
+}
+
